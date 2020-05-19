0c95d95a8df617ed8a570f3ebc76647eadc288f6
Ahmad Damra <ahmeddamra_cs@hotmail.com>
2020-05-19 03:39:57 +0300

    Auth Repo

---
+++ BookstoreAPI/Contracts/IAuthorRepository.cs
@@ -0,0 +1,13 @@
+﻿using BookstoreAPI.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookstoreAPI.Contracts
+{
+   public interface IAuthorRepository : IRepositoryBase<Author>
+    {
+
+    }
+}
